d20aca800f5c9aff526f7ab72a9ad1a1e33d52d0
Hannes Schaller <admin@cyberkov.at>
2016-10-15 00:28:47 +0200

    adding docker-compose file for reference

---
+++ hadash/ha_conf.rb
@@ -0,0 +1,8 @@
+$ha_url = "http://192.168.1.4:8123"
+$ha_apikey = ""
+
+$news_feeds = {
+#  "Traffic" => "http://api.sr.se/api/rss/traffic/2863",
+  "News" => "http://derStandard.at/?page=rss&ressort=Web",
+}
+
