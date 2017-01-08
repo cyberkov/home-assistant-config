0b18e0acf5a09400fc4b63096b68da2ce374dbd4
Hannes Schaller <admin@cyberkov.at>
2017-01-08 14:50:07 +0100

    change to https

--- hadash/ha_conf.rb
+++ hadash/ha_conf.rb
@@ -1,8 +1,7 @@
-$ha_url = "http://192.168.1.4:8123"
-$ha_apikey = ""
+$ha_url = 'https://192.168.1.4:8123'
+$ha_apikey = ''
 
 $news_feeds = {
-#  "Traffic" => "http://api.sr.se/api/rss/traffic/2863",
-  "News" => "http://derStandard.at/?page=rss&ressort=Web",
+  #  "Traffic" => "http://api.sr.se/api/rss/traffic/2863",
+  #  "News" => "http://derStandard.at/?page=rss&ressort=Web",
 }
-
