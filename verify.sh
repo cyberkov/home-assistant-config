d97fdd37db11c9069de16f7d9d737b261617b1bf
Hannes Schaller <admin@cyberkov.at>
2017-06-18 16:28:44 +0200

    update volume list for prod check

--- verify.sh
+++ verify.sh
@@ -2,4 +2,4 @@
 if [ ! -e "secrets.yaml" ]; then
   ln -s secrets_redacted.yaml secrets.yaml
 fi
-docker run -v "${PWD}:/config" homeassistant/home-assistant:latest /bin/sh -c "python -m homeassistant --config /config --script check_config"
+docker run -v "${PWD}:/config" -v "${PWD}/.travis:/etc/letsencrypt/live/graph.cyberkov.at" homeassistant/home-assistant:latest /bin/sh -c "python -m homeassistant --config /config --script check_config"
