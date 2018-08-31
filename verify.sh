493dc3673f19a81e998ee26f622e50c515ec5657
Hannes Schaller <johannes.schaller@apa.at>
2018-09-01 00:15:06 +0200

    fix tests

--- verify.sh
+++ verify.sh
@@ -3,4 +3,3 @@ if [ ! -e "secrets.yaml" ]; then
   ln -s secrets_redacted.yaml secrets.yaml
 fi
 docker run --rm -v "${PWD}:/config" -v "${PWD}/.travis:/etc/letsencrypt/live/graph.cyberkov.at" homeassistant/home-assistant:latest /bin/sh -c "python -m homeassistant --config /config --script check_config --info light"
-#docker run --rm -v "${PWD}:/config" -v "${PWD}/.travis:/etc/letsencrypt/live/graph.cyberkov.at" homeassistant/home-assistant:0.59.2 /bin/sh -c "python -m homeassistant --config /config --script check_config --info light"
