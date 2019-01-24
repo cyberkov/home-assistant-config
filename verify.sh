bd98fa6c1259d2d68ec2423f9abd6be4e9b29417
Hannes Schaller <johannes.schaller@apa.at>
2019-01-24 22:47:57 +0100

    updates for 0.86

--- verify.sh
+++ verify.sh
@@ -2,4 +2,4 @@
 if [ ! -e "secrets.yaml" ]; then
   ln -s secrets_redacted.yaml secrets.yaml
 fi
-docker run --rm -v "${PWD}:/config" -v "${PWD}/.travis:/etc/letsencrypt/live/graph.cyberkov.at" homeassistant/home-assistant:latest /bin/sh -c "python -m homeassistant --config /config --script check_config --info light"
+docker run --rm -v "${PWD}:/config:ro" -v "${PWD}/.travis:/etc/letsencrypt/live/graph.cyberkov.at" homeassistant/home-assistant:latest /bin/sh -c "python -m homeassistant --config /config --script check_config --info light"
